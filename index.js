041b0b2f0cc78d8a53b13193a81b637f470a14fe
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2022-07-20 21:01:17 +0200

    Slide 13

--- index.js
+++ index.js
@@ -9,33 +9,12 @@ function update() {
     // MOVE SNAKE HEAD
     // Only move every 30 frames.
     if (FRAME % 30 === 0) {
-        // MOVE SNAKE BODIES
-        const headEl = document.getElementById("snake-head");
-        const bodyEls = document.getElementsByClassName("snake-body");
-
-        // Loop through body elements BACKWARDS.
-        for (let i = bodyEls.length - 1; i >= 0; i--) {
-            const bodyEl = bodyEls[i];
-            const prevBodyEl = bodyEls[i - 1] || headEl;
-
-            // Set position of previous body element, or of head element.
-            bodyEl.style.left = prevBodyEl.style.left;
-            bodyEl.style.top = prevBodyEl.style.top;
-        }
-
+        moveSnakeBodies();
         // Note, that we move the BODIES first and the HEAD afterwards.
         // This allows for the first body to copy the OLD position of the head,
         // then when the head updates it moves to its NEW position.
         // This way, the body elements will "lag behind" by one position.
-
-        let x = parseInt(headEl.style.left) || 0;
-        let y = parseInt(headEl.style.top) || 0;
-
-        // Only move right for now...
-        x += 32;
-
-        headEl.style.left = `${x}px`;
-        headEl.style.top = `${y}px`;
+        moveSnakeHead();
     }
 
     // Call update() again once the browser
@@ -43,6 +22,34 @@ function update() {
     window.requestAnimationFrame(update);
 
     FRAME++;
+}
+
+function moveSnakeHead() {
+    const headEl = document.getElementById("snake-head");
+
+    let x = parseInt(headEl.style.left) || 0;
+    let y = parseInt(headEl.style.top) || 0;
+
+    // Only move right for now...
+    x += 32;
+
+    headEl.style.left = `${x}px`;
+    headEl.style.top = `${y}px`;
+}
+
+function moveSnakeBodies() {
+    const headEl = document.getElementById("snake-head");
+    const bodyEls = document.getElementsByClassName("snake-body");
+
+    // Loop through body elements BACKWARDS.
+    for (let i = bodyEls.length - 1; i >= 0; i--) {
+        const bodyEl = bodyEls[i];
+        const prevBodyEl = bodyEls[i - 1] || headEl;
+
+        // Set position of previous body element, or of head element.
+        bodyEl.style.left = prevBodyEl.style.left;
+        bodyEl.style.top = prevBodyEl.style.top;
+    }
 }
 
 // Start our game once the page has loaded.
